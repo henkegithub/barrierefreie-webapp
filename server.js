aaa06ecf8ba84801fd04f6ee5ea4a062bbd50cac
henkegithub <michaelh1995@gmx.de>
2024-08-09 15:05:16 +0200

    saved server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('*', (req, res) => {  // 'req' wird hier deklariert, aber nicht verwendet
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
